2f0e49db6e0cfe0351b34906d4594cb1886bf8cc
Tako-San <dxe13@mail.ru>
2021-05-10 18:14:49 +0300

    work with files

--- 4_SecretInfo/main.cc
+++ 4_SecretInfo/main.cc
@@ -1,16 +1,49 @@
+#include <filesystem>
+#include <fstream>
+#include <string>
+
 #include "AhoCor.hh"
 
-int main()
+namespace fs = std::filesystem;
+
+int main(int argc, char ** argv)
 {
+  if (argc < 2)
+  {
+    cout << "USAGE: " << argv[0] << " FILENAME" << endl;
+    return 1;
+  }
+
   Automaton ac;
 
-  ac.add_str("test");
-  ac.add_str("rok");
-  ac.add_str("roka");
-  ac.add_str("strok");
-  ac.add_str("t");
+  for (auto & file : fs::directory_iterator("test/vault"))
+  {
+    std::ifstream fst{file.path()};
+    std::string str;
 
-  ac.search("testovaya_stroka!");
+    while(fst)
+    {
+      std::string tmp;
+      fst >> tmp;
+      str.append(tmp);
+    }
+
+    fst.close();
+    ac.add_str(str);
+  }
+
+  fs::path pth{argv[1]};
+  std::fstream fst{pth};
+
+  std::string str;
+  while(fst)
+  {
+    std::string tmp;
+    fst >> tmp;
+    str.append(tmp);
+  }
+
+  ac.search(str);
 
   return 0;
 }
