754b1654d8f94508b85838e94de53dd16b49dec5
Tako-San <dxe13@mail.ru>
2021-05-12 16:44:48 +0300

    template functions out

--- 4_SecretInfo/AhoCor.hh
+++ 4_SecretInfo/AhoCor.hh
@@ -45,53 +45,59 @@ public:
   void add_str(const std::string &str);
   void add_from_file(const fs::path &p);
 
-  template <typename It> void search(It beg, It end)
-  {
-    cur_state_ = &root_;
-
-    for (; beg != end; ++beg)
-    {
-      std::cout << *beg << ':' << std::endl;
-      step(*beg);
-      print_if_term();
-    }
-  }
+  template <typename It> void search(It beg, It end);
 
 private:
   void step(char c);
   void print_if_term() const;
   static void print_msg(const std::string &str);
 
-  template <typename It> void add_str_noinit(It beg, It end, typename std::iterator_traits<It>::difference_type diff)
+  template <typename It> void add_str_noinit(It beg, It end, typename std::iterator_traits<It>::difference_type diff);
+};
+
+template <typename It> void Automaton::search(It beg, It end)
+{
+  cur_state_ = &root_;
+
+  for (; beg != end; ++beg)
   {
-    std::vector<TrieNode *> cur_nodes;
+    std::cout << *beg << ':' << std::endl;
+    step(*beg);
+    print_if_term();
+  }
+}
 
-    size_t wnum = diff - MIN_FILE_LEN + 1;
-    size_t cter = 0; // counter
+template <typename It>
+void Automaton::add_str_noinit(It beg, It end, typename std::iterator_traits<It>::difference_type diff)
+{
+  std::vector<TrieNode *> cur_nodes;
 
-    size_t counter = 0;
-    for (; beg != end; ++beg, ++counter)
+  size_t wnum = diff - MIN_FILE_LEN + 1;
+  size_t cter = 0; // counter
+
+  size_t counter = 1;
+  for (; beg != end; ++beg, ++counter)
+  {
+    if (cter < wnum)
     {
-      if (cter < wnum)
-      {
-        cur_nodes.push_back(&root_);
-        cter++;
-      }
+      cur_nodes.push_back(&root_);
+      cter++;
+    }
 
-      for (size_t i = 0, end = cur_nodes.size(); i < end; ++i)
+    for (size_t i = 0, end = cur_nodes.size(); i < end; ++i)
+    {
+      auto child_node = cur_nodes[i]->get_link(*beg);
+      if (!child_node)
       {
-        auto child_node = cur_nodes[i]->get_link(*beg);
-        if (!child_node)
-        {
-          child_node = new TrieNode(&root_);
-          cur_nodes[i]->links_[*beg] = child_node;
-        }
-        cur_nodes[i] = child_node;
-
-        if (counter - i + 1 >= MIN_FILE_LEN)
-          cur_nodes[i]->out = words_.size();
+        child_node = new TrieNode(&root_);
+        cur_nodes[i]->links_[*beg] = child_node;
       }
+      cur_nodes[i] = child_node;
+
+      if (counter - i >= MIN_FILE_LEN)
+        cur_nodes[i]->out = words_.size();
     }
   }
-};
+}
+
 } // namespace AhoCorasick
